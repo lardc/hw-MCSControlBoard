d5538251c770fea8044a5ace8e535710fb5edfbf
asytyi <alexander.a.sytyi@gmail.com>
2026-06-10 17:34:29 +0300

    Настройка для работы с UART2

--- Bootloader/Source/BoardConfig.h
+++ Bootloader/Source/BoardConfig.h
@@ -10,13 +10,14 @@
 #include "ZwRCC.h"
 #include "ZwGPIO.h"
 #include "ZwNCAN.h"
-#include "ZwSCI.h"
+#include "ZwUSART.h"
 #include "ZwTIM.h"
 #include "ZwIWDG.h"
 #include "ZwNFLASH.h"
 
 // Definitions
-//
+#define CONFIG_USE_USART2
+
 // Blinking LED settings 
 #define LED_BLINK_PORT		GPIOA
 #define LED_BLINK_PIN		Pin_0
